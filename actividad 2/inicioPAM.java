b12435ea7c013de2e8dc2a0b0c31a7f7668746fd
124050142 <124050142@upq.edu.mx>
2025-09-05 23:19:44 -0600

    Creamos la class InicioPAM

---
+++ actividad 2/inicioPAM.java
@@ -0,0 +1,46 @@
+public class inicioPAM{
+    public String ReglamentoPOO(){
+        return "Participacion Activa en clase.\n Trabajos de Classroom.\n
+         Entregas Completas.\n Respetar Tiempo de Entrega.\n Trabajos de Calidad.";
+    }
+
+    public String LineamientosClassroom(){
+        return "1. Se requiere 80% de asistencia para tener derecho a evaluacion de parcial
+        y 80% de trabajos en clase.\n 
+        2. Se permiten 10 minutos de tolerancia y si el alumno llega despues de este tiempo puede permanecer en la clase, pero no se tomará la asistencia (Solamente en los horarios de inicio: 7:00a.m y 14:00p.m).\n
+        3.Las faltas deberán estar justificadas mediante el correo institucional con un plazo maximo de 24 horas posteriores a la hora e falta en clase mediante correo del tutor(a), justificantes entregados fuera de la fecha límite permitido no se aceptan, únicamente se aceptarán recetas médicas y citatorios jurídicos. (De forma física deben ser presentados al tutor para ser validos y de forma posterior emitidos).\n 
+        4.Las tareas y trabajos deberán subirlas al classroom de forma individual y no se recibirán de manera extemporánea. (Salvo previo justificante validado por el tutor)\n 
+        5.Las tareas y trabajos presentarlos en tiempo y forma. La demora de un trabajo o tarea sin justificante y/o con justificante fuera del límite no se aceptan.\n
+        6.Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.\n
+        7.El plagio o copia de trabajos y/o exámenes, será condicionado a reprobar a la asignatura y se reportará al área correspondiente.\n
+        8.Cualquier deshonestidad académica será sancionada reprobando el parcial sin derecho a examen final.\n
+        9.En circunstancia de indisciplina o falta de respeto por parte del alumno hacia docentes, administrativos, compañeros
+        o cualquier persona perteneciente a la universidad, se realizará una primera llamada de atención, si el alumno hace caso
+        omiso tendrá que abandonar el aula, tres incidencias de este tipo el alumno no tendrá derecho a examen final o parcial.\n
+        10.Uso de laptops y/o dispositivos móviles quedará limitados a uso exclusivo de las actividades que así lo requieran.\n
+        11.Prohibido el uso de audifonos durante clase.\n
+        12.Prohibido comer y/o tomar líquidos en el salón.\n
+        13.Prohibido sentarse encima de las mesas, así como columpiarse en las sillas.\n
+        14.Todo tema académico debe ser revisado primeramente por parte del alumno con el docente, de no resolverse,
+        pasar con su respectivo tutor, y de ser necesario con la coordinación de tutores. En caso de no solucionarse pasar
+        a la dirección del programa educativo (debe mantenerse este seguimiento de forma obligatoria)\n
+        15.Cualquier situación no prevista en el presente reglamento pasar directamente con la dirección del programa educativo.\n
+        16.El día destinado a entrega de calificaciones todos los estudiantes deben estar presentes, ese día se entregarán exámenes y 
+        se brindará retroalimentación.\n
+        17.Este reglamento entra en vigor después de se firme o se acepta por la mayoría de los estudiantes asistentes a la primera sesión
+        de la materia, una vez firmado o aceptado por el 50 % mas el jefe de grupo, es vigente para todo alumno inscrito en el curso aunque no esté
+        presente en la primera sesión.\n";
+        }
+
+     public String FechasdeParciales(){
+        return "1er Parcial: 29/09/25\n
+        2do Parcial: 03/11/25\n
+        3er Parcial: 01/12/25\n
+        Final: 08/12/25";
+
+     }
+     public String PorcentajesporParcial(){
+        return "1P\n Cocimento:40%\n Desempeño:20%\n Producto:30%\n Pi:10%\n\n2P\n Conocimiento:40%\n Desempeño:20%\n Producto:20%\n Pi:20%\n\n3P\n Conocimiento:20%\n Desempeño:10%\n Producto:40%\n Pi:30%";
+        
+     }
+    }
